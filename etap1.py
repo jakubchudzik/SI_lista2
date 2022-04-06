9cdc9b10bb86cda2d42c8e54a5a50410d6d38f32
jakubchudzik <Chkuba200.pl>
2022-04-07 01:36:20 +0200

    solver done

--- etap1.py
+++ etap1.py
@@ -84,6 +84,95 @@ def b_make_grid(file_name):
     _matrix = np.array(lista)
     return  _matrix
 
+def f_make_grid(file_name):
+    with open(file_name, 'r') as file:
+        binary = file.readlines()
+
+    lista = []
+
+    for i in range(len(binary)):
+        binary[i] = binary[i].strip()
+        pom = []
+        for j in binary[i]:
+            if j == "x":
+                pom.append('')
+            else:
+                pom.append(j)
+            if i % 2 ==1:
+                pom.append('-')
+        if i%2==1:
+            pom.pop()
+        lista.append(np.array(pom))
+    _matrix = np.array(lista)
+    return  _matrix
+
+def f_make_field(_matrix):
+    field = []
+    for i in range(len(_matrix)//2+1):
+        field.append(f'{i+1}')
+    return field
+
+# def find_free_place(_matrix):
+#     for i in range(len(_matrix),2):
+#         for j in range(len(_matrix[i]),2):
+#             if _matrix[i][j] == '':
+#                 return (j,i)
+#     return False
+
+def f_is_ok_row(_matrix,field,coords):
+    liczniki={}
+    for i in range(0,len(_matrix),2):
+        if _matrix[coords[1]][i] in field:
+            if _matrix[coords[1]][i] not in liczniki:
+                liczniki[_matrix[coords[1]][i]] = 1
+            else:
+                return False
+    return True
+
+def f_is_ok_col(_matrix,field,coords):
+    liczniki = {}
+    for i in range(0,len(_matrix),2):
+        if _matrix[i][coords[0]] in field:
+            if _matrix[i][coords[0]] not in liczniki:
+                liczniki[_matrix[i][coords[0]]] = 1
+            else:
+                return False
+    return True
+
+def f_is_ok(a,b,character):
+    if a=='' or b == '':
+        return True
+    if character == ">":
+        if int(a)>int(b):
+            return True
+        else:
+            return False
+    if character == "<":
+        if int(a)<int(b):
+            return True
+        else:
+            return False
+    return True
+def f_is_ok_with_sanctions(_matrix,coords):
+    row = coords[1]
+    col = coords[0]
+    pom = []
+    for i in range(len(_matrix)):
+        pom.append(_matrix[row][i])
+    k=0
+    for i in range(len(pom)//2):
+       if not f_is_ok(pom[k],pom[k+2],pom[k+1]):
+           return False
+       k+=2
+    pom=[]
+    for i in range(len(_matrix)):
+        pom.append(_matrix[i][col])
+    k = 0
+    for i in range(len(pom) // 2):
+        if not f_is_ok(pom[k], pom[k + 2], pom[k + 1]):
+            return False
+        k += 2
+    return True
 
 def find_free_place(_matrix):
     for i in range(len(_matrix)):
@@ -91,9 +180,12 @@ def find_free_place(_matrix):
             if _matrix[i][j] == '':
                 return (j, i)
     return False
-
 def binary(_matrix,field,coords):
     if b_is_less_than_3_char(_matrix, coords, field) and b_is_different(_matrix, coords) and b_isOkRowNumbers(_matrix, len(_matrix) // 2, field,coords) and b_isOkColNumbers(_matrix, len(_matrix) // 2,field, coords):
+        return True
+    return False
+def futo(_matrix,field,coords):
+    if f_is_ok_row(_matrix, field, coords) and f_is_ok_col(_matrix, field, coords) and f_is_ok_with_sanctions(_matrix,coords):
         return True
     return False
 
@@ -110,12 +202,8 @@ def solver(_matrix,field,function,coords=None):
                 solver(_matrix,field,function,free)
     _matrix[coords[1]][coords[0]]=''
 
-
-
 def main():
-
     field1 = ['0', '1']
-
     print('Binary 6x6')
     _matrix = b_make_grid("data/binary_6x6")
     solver(_matrix,field1,binary)
@@ -127,6 +215,9 @@ def main():
     print('Binary 10x10')
     _matrix = b_make_grid("data/binary_10x10")
     solver(_matrix,field1,binary)
-
+    print('------------------------')
+    _matrix = f_make_grid("data/futoshiki_4x4")
+    solver(_matrix,f_make_field(_matrix),futo)
+    _matrix = f_make_grid("data/futoshiki_5x5")
+    solver(_matrix, f_make_field(_matrix), futo)
 main()
-
--- futoshiki.py
+++ futoshiki.py
@@ -1,9 +1,7 @@
-import copy
 import time
 
 import numpy as np
-p = 0
-def make_grid(file_name):
+def f_make_grid(file_name):
     with open(file_name, 'r') as file:
         binary = file.readlines()
 
@@ -18,27 +16,27 @@ def make_grid(file_name):
             else:
                 pom.append(j)
             if i % 2 ==1:
-                pom.append('')
+                pom.append('-')
         if i%2==1:
             pom.pop()
         lista.append(np.array(pom))
     _matrix = np.array(lista)
     return  _matrix
 
-def make_field(_matrix):
+def f_make_field(_matrix):
     field = []
     for i in range(len(_matrix)//2+1):
         field.append(f'{i+1}')
     return field
 
-def find_free_place(_matrix):
-    for i in range(0,len(_matrix),2):
-        for j in range(0,len(_matrix[i]),2):
-            if _matrix[i][j] == '':
-                return (j,i)
-    return False
+# def find_free_place(_matrix):
+#     for i in range(len(_matrix),2):
+#         for j in range(len(_matrix[i]),2):
+#             if _matrix[i][j] == '':
+#                 return (j,i)
+#     return False
 
-def is_ok_row(_matrix,field,coords):
+def f_is_ok_row(_matrix,field,coords):
     liczniki={}
     for i in range(0,len(_matrix),2):
         if _matrix[coords[1]][i] in field:
@@ -48,7 +46,7 @@ def is_ok_row(_matrix,field,coords):
                 return False
     return True
 
-def is_ok_col(_matrix,field,coords):
+def f_is_ok_col(_matrix,field,coords):
     liczniki = {}
     for i in range(0,len(_matrix),2):
         if _matrix[i][coords[0]] in field:
@@ -58,7 +56,7 @@ def is_ok_col(_matrix,field,coords):
                 return False
     return True
 
-def is_ok(a,b,character):
+def f_is_ok(a,b,character):
     if a=='' or b == '':
         return True
     if character == ">":
@@ -72,7 +70,7 @@ def is_ok(a,b,character):
         else:
             return False
     return True
-def is_ok_with_sanctions(_matrix,coords):
+def f_is_ok_with_sanctions(_matrix,coords):
     row = coords[1]
     col = coords[0]
     pom = []
@@ -80,7 +78,7 @@ def is_ok_with_sanctions(_matrix,coords):
         pom.append(_matrix[row][i])
     k=0
     for i in range(len(pom)//2):
-       if not is_ok(pom[k],pom[k+2],pom[k+1]):
+       if not f_is_ok(pom[k],pom[k+2],pom[k+1]):
            return False
        k+=2
     pom=[]
@@ -88,33 +86,34 @@ def is_ok_with_sanctions(_matrix,coords):
         pom.append(_matrix[i][col])
     k = 0
     for i in range(len(pom) // 2):
-        if not is_ok(pom[k], pom[k + 2], pom[k + 1]):
+        if not f_is_ok(pom[k], pom[k + 2], pom[k + 1]):
             return False
         k += 2
     return True
+def futo(_matrix,field,coords):
+    if f_is_ok_row(_matrix, field, coords) and f_is_ok_col(_matrix, field, coords) and f_is_ok_with_sanctions(_matrix,coords):
+        return True
+    return False
 
-def rec_futo(_matrix,field,coords):
-    global p
+def solver(_matrix,field,function,coords=None):
+    if coords == None:
+        coords = find_free_place(_matrix)
     for i in field:
         _matrix[coords[1]][coords[0]] = i
-        if is_ok_row(_matrix,field,coords) and is_ok_col(_matrix,field,coords) and is_ok_with_sanctions(_matrix,coords):
+        if function(_matrix,field,coords):
             free = find_free_place(_matrix)
             if free == False:
-                p+=1
-                print(p)
                 print(_matrix)
-                print()
             else:
-                rec_futo(_matrix,field,find_free_place(_matrix))
+                solver(_matrix,field,futo,find_free_place(_matrix))
     _matrix[coords[1]][coords[0]] = ''
+
 
 
 def main():
     t1 = time.time()
-    _matrix = make_grid("data/futoshiki_6x6")
-    rec_futo(_matrix,make_field(_matrix),find_free_place(_matrix))
-    # print(is_ok_with_sanctions(_matrix,(0,4)))
-    print("Wynik: ")
-    print(p)
-    print(time.time()-t1)
+    _matrix = f_make_grid("data/futoshiki_4x4")
+    #print(_matrix)
+    solver(_matrix,f_make_field(_matrix),futo)
+    # print(f_is_ok_with_sanctions(_matrix,(0,4)))
 main()
